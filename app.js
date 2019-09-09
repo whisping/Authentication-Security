1736e080317b4c43984c099228f9c274dea0efee
whisping <whisping@gmail.com>
2019-09-09 18:22:51 +0300

    3. Hashing Passwords

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 const app = express();
 
@@ -17,8 +17,6 @@ const userSchema = new mongoose.Schema ({
     email: String,
     password: String
 });
-
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
 const User = mongoose.model("User", userSchema);
 
@@ -47,7 +45,7 @@ app.get("/register", function(req, res) {
 app.post("/register", function(req, res) {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
     newUser.save(function(err) {
         if (err) {
@@ -61,7 +59,7 @@ app.post("/register", function(req, res) {
 app.post("/login", function(req, res) {
     const user = {
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     }
 
     User.findOne({email: user.email}, function(err, foundUser) {
@@ -71,6 +69,8 @@ app.post("/login", function(req, res) {
             if (foundUser) {
                 if (foundUser.password === user.password) {
                     res.render("secrets");
+                }   else {
+                    res.send("Wrong password!");
                 }
             }
         }
