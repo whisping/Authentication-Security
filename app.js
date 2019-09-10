4568e042abb2fbf1de4a3485da3bdbaaf1b6a0ff
whisping <whisping@gmail.com>
2019-09-10 14:25:42 +0300

    6. OAuth 2.0 & How to Implement Sign In w/ Google

--- app.js
+++ app.js
@@ -8,15 +8,19 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 
 const app = express();
 
-const userSchema = new mongoose.Schema ({
+const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = mongoose.model("User", userSchema);
 
@@ -37,8 +41,31 @@ mongoose.set('useCreateIndex', true);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done){
+    done(null, user.id);
+});
+passport.deserializeUser(function(id, done){
+    User.findById(id, function(err, user){
+        done(err, user);
+    })
+});
+
+passport.use(new GoogleStrategy({
+        clientID: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        callbackURL: "http://localhost:3000/auth/google/secrets",
+        userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+    },
+    function(accessToken, refreshToken, profile, cb) {
+        console.log(profile);
+
+        User.findOrCreate({
+            googleId: profile.id
+        }, function(err, user) {
+            return cb(err, user);
+        });
+    }
+));
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -64,49 +91,62 @@ app.get("/register", function(req, res) {
     res.render("register");
 });
 
-app.get("/secrets", function(req, res){
+app.get("/secrets", function(req, res) {
     if (req.isAuthenticated()) {
         res.render("secrets");
-    }   else {
+    } else {
         res.redirect("/login")
     }
 });
 
-app.get("/logout", function(req, res){
+app.get("/logout", function(req, res) {
     req.logout();
     res.redirect("/");
 })
 
-app.post("/register", function(req, res) {
+app.get("/auth/google",
+    passport.authenticate("google", {
+        scope: ["profile"]
+    })
+    // (req, res, function(){
+    //     res.redirect("/secrets");
+    // })
+);
 
-    User.register({username: req.body.username}, req.body.password, function(err, user){
+app.get("/auth/google/secrets", passport.authenticate("google", {
+        failureRedirect: "/login"
+    }),
+    function(req, res) {
+        res.redirect("/secrets")
+    });
+
+app.post("/register", function(req, res) {
+    User.register({
+        username: req.body.username
+    }, req.body.password, function(err, user) {
         if (err) {
             console.log(err);
             res.redirect("/register");
-        }   else {
-            passport.authenticate("local")(req, res, function(){
+        } else {
+            passport.authenticate("local")(req, res, function() {
                 res.redirect("/secrets");
             })
         }
     })
-
 })
 
 app.post("/login", function(req, res) {
-
     const user = new User({
         username: req.body.username,
         password: req.body.password
     })
-
-    req.login(user, function(err){
+    req.login(user, function(err) {
         if (err) {
             console.log(err);
-        }   else {
-            passport.authenticate("local")(req, res, function(){
+        } else {
+            passport.authenticate("local")(req, res, function() {
                 res.redirect("/secrets");
             })
         }
     })
-
 })
